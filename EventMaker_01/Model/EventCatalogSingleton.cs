2074cb1b3e08bfc4647a8555993352ff9f35a9ab
Rolf Petersen <rollep7@msn.com>
2017-02-01 16:40:45 +0100

    Opgave 1, 7

---
+++ EventMaker_01/Model/EventCatalogSingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMaker_01.Model
+{
+    class EventCatalogSingleton
+    {
+
+    
+        public ObservableCollection<Event> Events { get; set; }
+
+        public EventCatalogSingleton()
+        {
+            Events = new ObservableCollection<Event>();
+            
+        }
+
+        public void AddEvent(int id,string name,string description,string place,DateTime dateTime)
+        {
+            Event nyEvent = new Event(id,name,description,place,dateTime);
+            Events.Add(nyEvent);
+
+        }
+    }
+}
---
+++ EventMaker_01/ViewModel/EventViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventMaker_01.Model;
+
+namespace EventMaker_01.ViewModel
+{
+    class EventViewModel
+    {
+        public EventCatalogSingleton EventCatalogSingleton { get; set; }
+    }
+}
