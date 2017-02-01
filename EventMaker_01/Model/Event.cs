5553e71c6209aa8f3ed7e126aca1c40bf4714f99
Rolf Petersen <rollep7@msn.com>
2017-02-01 15:03:46 +0100

    Event class med prop.

---
+++ EventMaker_01/Model/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMaker_01.Model
+{
+    class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public Event(int id,string name,string description,string place,DateTime dateTime)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Place = place;
+            this.DateTime = dateTime;
+        }
+    }
+}
