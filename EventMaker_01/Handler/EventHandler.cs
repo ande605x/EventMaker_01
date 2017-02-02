d03365c6fe8db6d053dcf1cd73e6754aaafaa6e4
Rolf Petersen <rollep7@msn.com>
2017-02-02 10:08:37 +0100

    Opgave 2,5 EventHandler

---
+++ EventMaker_01/Handler/EventHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventMaker_01.Converter;
+using EventMaker_01.ViewModel;
+
+namespace EventMaker_01.Handler
+{
+    class EventHandler
+    {
+        public EventViewModel EventViewModel { get; set; }
+
+
+        public EventHandler(EventViewModel eventViewModel)
+        {
+            EventViewModel = eventViewModel;
+        }
+
+        public void CreateEvent()
+        {
+            EventViewModel.EventCatalogSingleton.AddEvent(EventViewModel.Id,
+                EventViewModel.Name,
+                EventViewModel.Description,
+                EventViewModel.Place,
+                DateTimeConverter.DateTimeOffsetAndTimeSetToDateTime(EventViewModel.Date, EventViewModel.Time));
+        }
+
+        
+    }
+}
